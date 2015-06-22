ecac5762c9a39bda8b4bddcf41602771a3b9c92e
Robert Rees <robert.rees@guardian.co.uk>
2015-06-22 16:52:55 +0100

    Restores some of the original page running functionality

--- test/runner.js
+++ test/runner.js
@@ -14,6 +14,9 @@ mocha.timeout(15 * 1000);
 mocha.timeout(1200000);
 mocha.reporter('spec');
 
+// Tests hang with this in place
+//mocha.addFile(path.resolve(__dirname, 'setup.js'));
+
 glob(__dirname + '**/*.spec.js', function (err, files){
   if(err){process.exit(1);}
 
